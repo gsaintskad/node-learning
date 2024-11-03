7d4280b9615c88f295239a538d77d8daa45b29a9
iwouldneverto <glebsmile3303@gmail.com>
2024-11-03 11:04:45 +0100

    "devDependencies": {     "@types/express": "^5.0.0",     "@types/node": "^22.7.7",     "ts-node": "^10.9.2",     "typescript": "^5.6.3"   },   "dependencies": {     "dotenv": "^16.4.5",     "express": "^4.21.1",     "nodemon": "^3.1.7",     "prettier": "^3.3.3"   }

--- index.ts
+++ index.ts
@@ -1,35 +1,37 @@
-import express, {NextFunction, Request, Response} from 'express';
-import dotenv from 'dotenv';
+import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
-const getRoot=(req: Request, res: Response, next:NextFunction) => {
-  res.send('Hello World!');
+const getRoot = (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
   next();
-}
-const getRootNext=(req: Request, res: Response) => {
-  console.log('This is the next middleware');
-}
+};
+const getRootNext = (req: Request, res: Response) => {
+  console.log("This is the next middleware");
+};
 
-const getComments=(req: Request, res: Response, next:NextFunction) => {
-  res.send('Hello Comments!');
+const getComments = (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello Comments!");
   next();
-}
-const getCommentsNext=(req: Request, res: Response) => {
-  console.log('This is the get comments next middleware');
-}
-const postComments=(req: Request, res: Response, next:NextFunction) => {
-  res.send('post comments!');
+};
+const getCommentsNext = (req: Request, res: Response) => {
+  console.log("This is the get comments next middleware");
+  console.log(req.params);
+};
+const postComments = (req: Request, res: Response, next: NextFunction) => {
+  res.send("post comments!");
   next();
-}
+};
 
-const postCommentsNext=(req: Request, res: Response) => {
-  console.log('This is the post comments next middleware');
-}
-app.get('/', getRoot,getRootNext);
-app.get('/comments', getComments,getCommentsNext);
-app.post('/comments', postComments,postCommentsNext);
+const postCommentsNext = (req: Request, res: Response) => {
+  console.log("This is the post comments next middleware");
+};
+app.get("/", getRoot, getRootNext);
+app.get("/comments", getComments, getCommentsNext);
+app.post("/comments", postComments, postCommentsNext);
+app.get("/comments/:id", getComments, getCommentsNext);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
