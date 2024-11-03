2d5a733b32437a0b57d847b8a0ab6dcd16dbb0dc
iwouldneverto <glebsmile3303@gmail.com>
2024-11-03 10:34:16 +0100

    0 express server

--- index.ts
+++ index.ts
@@ -1,20 +1,14 @@
-const fs=require('fs/promises');
-const a=5;
-fs.writeFile('./text.txt',`${a} text`)
-    .then(()=>{console.log(a.toString() +" has been written successfully!")})
-    .then(()=>{fs.appendFile('./text.txt',`\n new line with a+1 :${a+1} text`)})
-    .then(()=>{console.log((a+1).toString() +" has been written successfully!")})
-    .catch((err:Error)=>{console.log(err)});
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
-async function fileWriting(){
-    await fs.writeFile('./text2.txt',`${a} text`);
-    await fs.appendFile('./text2.txt',`\n\n\n${a+1} text`);
-    console.log(`${a+1} has been written successfully!`);
-}
+const app = express();
 
-try{
-    fileWriting();
-}
-catch(err){
-    console.error(err);
-}
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
